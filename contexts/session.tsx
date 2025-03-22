92a4b06b8189a5d4a0d49f2637cd9b3e45afaa90
Le An Nguyen <nguyenlean96@gmail.com>
2025-03-22 12:38:26 -0400

    Add session context as a guard to control access to the main app

---
+++ contexts/session.tsx
@@ -0,0 +1,113 @@
+import React, {
+  useContext,
+  createContext,
+  type PropsWithChildren,
+  useEffect,
+} from 'react';
+import { makeRedirectUri } from 'expo-auth-session';
+import { useRouter } from 'expo-router';
+import { useStorageState } from '@/hooks/useStorageState';
+import * as Linking from 'expo-linking';
+import * as QueryParams from 'expo-auth-session/build/QueryParams';
+import { supabase } from '@/services/supabase'
+import { SupabaseGoogleOauth } from '@/hooks/supabase/auth/oauth/OauthWithGoogle';
+import { SupabaseEmailOtpAuth } from '@/hooks/supabase/auth/otp/OauthWithEmail';
+
+
+const AuthContext = createContext<any>({});
+
+// This hook can be used to access the user info.
+export function useSession() {
+  const value = useContext(AuthContext);
+  if (process.env.NODE_ENV !== 'production') {
+    if (!value) {
+      throw new Error('useSession must be wrapped in a <SessionProvider />');
+    }
+  }
+
+  return value;
+}
+
+export function SessionProvider({ children }: PropsWithChildren) {
+  const router = useRouter();
+  const [
+    [sessionLoading, session],
+    setSession
+  ] = useStorageState('session');
+
+  // Handle linking into app from email app.
+  const redirectTo = makeRedirectUri({
+    path: '/signin'
+  });
+  const url = Linking.useURL()
+
+  const createSessionFromUrl = async (url: string) => {
+    const { params, errorCode } = QueryParams.getQueryParams(url)
+
+    if (errorCode) throw new Error(errorCode)
+    const { access_token, refresh_token } = params
+
+    if (!access_token) return
+
+    const { data, error } = await supabase.auth.setSession({
+      access_token,
+      refresh_token,
+    })
+    console.log('data', data)
+    if (error) throw error
+    return data.session
+  }
+
+  const { error: oauthGoogleError, oauthWithGoogle } = SupabaseGoogleOauth(
+    redirectTo,
+    createSessionFromUrl,
+  );
+  const { sendAuthEmailOtp } = SupabaseEmailOtpAuth(redirectTo);
+
+  if (url) createSessionFromUrl(url)
+  useEffect(() => {
+    if (session) {
+      router.replace('/(auth)/home')
+    } else {
+      router.replace('/signin')
+    }
+  }, [session]);
+
+  useEffect(() => {
+    const handleDeepLink = async (event: any) => {
+      const url = event.url
+      if (url) {
+        if (url.includes('access_denied')) {
+          router.replace('/signin')
+        }
+        try {
+          const sessionData = await createSessionFromUrl(url)
+          setSession(JSON.stringify(sessionData))
+        } catch (error) {
+          console.error('error', error)
+        }
+      }
+    }
+
+    const subscription = Linking.addEventListener('url', handleDeepLink)
+
+    return () => {
+      subscription.remove()
+    }
+  }, [])
+
+  const signOut = () => {
+    setSession(null);
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        sendAuthEmailOtp,
+        oauthWithGoogle,
+        signOut,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
