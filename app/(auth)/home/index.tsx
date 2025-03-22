452a55d81646b735326e1890f722f65daad74275
Le An Nguyen <nguyenlean96@gmail.com>
2025-03-22 15:21:24 -0400

    Implement the `@shopify/flashlist`

--- app/(auth)/home/index.tsx
+++ app/(auth)/home/index.tsx
@@ -1,17 +1,96 @@
-import { Text, View } from "react-native";
-import { usePathname } from "expo-router";
+import { useRef, useState, useReducer, useEffect } from 'react';
+import {
+  View,
+} from 'react-native';
+import { FlashList } from "@shopify/flash-list";
+import { job_posting_examples } from '@/constants/job_posting_example';
+import { RefreshControl } from 'react-native-gesture-handler';
+import ScrollCard from '@/components/ui/jobs/scroll/card';
 
-export default function Index() {
-  const pathname = usePathname();
+function handleLike(
+  state: Set<number>,
+  action: { type: 'like' | 'unlike', payload: number }
+): Set<number> {
+  switch (action.type) {
+    case 'like':
+      state.add(action.payload);
+      return new Set(state);
+    case 'unlike':
+      state.delete(action.payload);
+      return new Set(state);
+    default:
+      return state;
+  }
+}
+
+export default function Home() {
+  const scrollRef = useRef(null);
+  const [refreshing, setRefreshing] = useState(false);
+  const [scrollToIndex, setScrollToIndex] = useState(0);
+
+  const [liked, setLiked] = useReducer(
+    handleLike,
+    new Set<number>()
+  )
+
+  function handlePressLike(jobId: number) {
+    if (liked.has(jobId)) {
+      setLiked({ type: 'unlike', payload: jobId });
+    } else {
+      setLiked({ type: 'like', payload: jobId });
+    }
+  }
+
+  const handleScrollToIndex = (index: number) => {
+    if (scrollRef.current) {
+      (scrollRef.current as any)
+        .scrollToItem({
+          item: job_posting_examples[index],
+          animated: true,
+        })
+    }
+  }
+
+  useEffect(() => {
+    if (!refreshing) {
+      handleScrollToIndex(scrollToIndex)
+    }
+  }, [refreshing])
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit <Text style={{ backgroundColor: '#ddd', color: 'blue', paddingHorizontal: 8 }}>{`app/(auth)${pathname}/index.tsx`}</Text> to edit this screen.</Text>
+    <View style={{
+      flex: 1,
+    }}>
+      <FlashList
+        ref={scrollRef}
+        initialScrollIndex={scrollToIndex}
+        style={{
+          flex: 1,
+        }}
+        data={refreshing ? [] : job_posting_examples}
+        renderItem={({ item, index }) => (
+          <ScrollCard
+            item={item}
+            handlePressLike={handlePressLike}
+            wasLiked={liked.has(item?.jobview?.job?.listingId)}
+          />
+        )}
+        keyExtractor={(item) => item?.jobview?.job?.listingId.toString()}
+        extraData={liked}
+        refreshing={refreshing}
+        refreshControl={
+          <RefreshControl refreshing={refreshing}
+            onRefresh={() => {
+              setRefreshing(true);
+              setTimeout(() => {
+                setRefreshing(false);
+              },
+                100
+              )
+            }}
+          />
+        }
+      />
     </View>
-  );
+  )
 }
