4fdc7a3f75de87306e77dac2bb487fbe6462e917
Le An Nguyen <nguyenlean96@gmail.com>
2025-03-25 12:13:08 -0400

    Removed unused style

--- components/ui/jobs/swipe/Profiles.tsx
+++ components/ui/jobs/swipe/Profiles.tsx
@@ -136,19 +136,6 @@ const styles = StyleSheet.create({
     justifyContent: 'space-evenly',
     padding: 16,
   },
-  circle: {
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    padding: 12,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    shadowColor: 'gray',
-    shadowOffset: { width: 1, height: 1 },
-    shadowOpacity: 0.18,
-    shadowRadius: 2,
-  },
 });
 
 export default Profiles;
